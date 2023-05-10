4bdaa7a9d88c26d0c9795485fc52665d53b5294c
jerry <>
2023-05-10 20:27:12 +0800

    no message

--- apps/chat/src/app/api/user/register/route.ts
+++ apps/chat/src/app/api/user/register/route.ts
@@ -21,57 +21,48 @@ export async function POST(req: NextRequest): Promise<Response> {
   try {
     const { email, password, code, code_type, phone, invitation_code } =
       await req.json();
- 
-   // const userDal = new UserDAL(testRedis);
-   const userDal = new UserDAL();
-    const ress = await userDal.exists(email);
-    if (ress) {
+
+    const userDal = new UserDAL();
+    if (await userDal.exists(email)) {
       // User already exists.
       return NextResponse.json({ status: ResponseStatus.alreadyExisted });
     }
 
-//     /* Activation verification code */
-    
-// if (ifVerifyCode) {
-//   const registerCodeLogic = new RegisterCodeLogic();
-//   const success = await registerCodeLogic.activateCode(email, code.trim());
+    /* Activation verification code */
+    if (ifVerifyCode) {
+      const registerCodeLogic = new RegisterCodeLogic();
+      const success = await registerCodeLogic.activateCode(email, code.trim());
 
-//   if (!success)
-//     return NextResponse.json({ status: ResponseStatus.invalidCode });
-// }
-//     const redis = new Redis({
-//       url: 'https://apn1-decent-bee-34619.upstash.io',
-//       token: 'AYc7ACQgNGY1N2YyZGEtMTM1MC00NDg1LTkxNGEtZjdkZDgzNGNiYzAwNzRlMzcwNTRlNDI0NDU5ZDgwNWUzNWE5OTQwOTM1OTU=',
-//     });
-       
+      if (!success)
+        return NextResponse.json({ status: ResponseStatus.invalidCode });
+    }
 
-//     const ress =await redis.json.set(email, "$", password);
-//     console.log(ress);
-//   // const user = new UserLogic();
-//   // await user.register(email, password);
-//     //process.exit(1);
-//     // If using an invitation code to register,
-//     // then determine the type of activation code and grant corresponding rights.
-// // if (invitation_code) {
-// //   const invitationCode = new InvitationCodeLogic();
+    const user = new UserLogic();
+    await user.register(email, password);
 
-// //   const code = await invitationCode.acceptCode(
-// //     email,
-// //     invitation_code.toLowerCase()
-// //   );
-// //   // await user.newSubscription({
-// //   //   startsAt: Date.now(),
-// //   //   endsAt: Date.now() + 3 * 60 * 60 * 24 * 1000,
-// //   //   plan: "pro",
-// //   //   tradeOrderId: `club-code-${invitation_code.toLowerCase()}`,
-// //   // });
-// // }
-//     // After registration, directly generate a JWT Token and return it.
-//     // const accessControl = new AccessControlLogic();
-//     // const token = await accessControl.newJWT(email);
-     return NextResponse.json({
-       status: ResponseStatus.Success,
-       sessionToken: ress,
+    // If using an invitation code to register,
+    // then determine the type of activation code and grant corresponding rights.
+    if (invitation_code) {
+      const invitationCode = new InvitationCodeLogic();
+
+      const code = await invitationCode.acceptCode(
+        email,
+        invitation_code.toLowerCase()
+      );
+      // await user.newSubscription({
+      //   startsAt: Date.now(),
+      //   endsAt: Date.now() + 3 * 60 * 60 * 24 * 1000,
+      //   plan: "pro",
+      //   tradeOrderId: `club-code-${invitation_code.toLowerCase()}`,
+      // });
+    }
+
+    // After registration, directly generate a JWT Token and return it.
+    const accessControl = new AccessControlLogic();
+    const token = await accessControl.newJWT(email);
+    return NextResponse.json({
+      status: ResponseStatus.Success,
+      sessionToken: token,
     });
   } catch (error) {
     console.error("[REGISTER]", error);
