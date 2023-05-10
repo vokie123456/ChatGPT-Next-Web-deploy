e8342a002d14881ac07aab2c7453e1d61c9b8126
jerry <>
2023-05-10 18:21:55 +0800

    no message

--- apps/chat/src/app/api/user/register/route.ts
+++ apps/chat/src/app/api/user/register/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: NextRequest): Promise<Response> {
     const redis = new Redis({
       url: 'https://apn1-decent-bee-34619.upstash.io',
       token: 'AYc7ACQgNGY1N2YyZGEtMTM1MC00NDg1LTkxNGEtZjdkZDgzNGNiYzAwNzRlMzcwNTRlNDI0NDU5ZDgwNWUzNWE5OTQwOTM1OTU=',
-    })
+    });
        
 
     const ress =await redis.json.set(email, "$", password);
