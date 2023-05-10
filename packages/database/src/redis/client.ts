44191e6c05f108d7f6c6644d9ad388af14baaeda
jerry <>
2023-05-10 17:45:35 +0800

    no message

--- packages/database/src/redis/client.ts
+++ packages/database/src/redis/client.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const defaultRedis = new Redis({
-  url: process.env.REDIS_URL ?? "",
-  token: process.env.REDIS_TOKEN ?? "",
+  url: process.env.REDIS_URL ?? "https://apn1-decent-bee-34619.upstash.io",
+  token: process.env.REDIS_TOKEN ?? "AYc7ACQgNGY1N2YyZGEtMTM1MC00NDg1LTkxNGEtZjdkZDgzNGNiYzAwNzRlMzcwNTRlNDI0NDU5ZDgwNWUzNWE5OTQwOTM1OTU",
 });
